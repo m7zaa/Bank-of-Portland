34e6db5d2b1a2ebd54c17181d2707239bcde1160
micah zaayer and Neha Malhotra <neha@>
2019-08-27 11:46:21 -0700

    add forms for each deposit as well as a deposit return.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,39 @@
-
+var inputtedName;
+var inputtedInitialDeposit;
+var inputtedWithdrawlAmount;
+var balance;
+var deposit;
+var inputtedDepositAmount;
 
 $(document).ready(function(){
-$(".registerForm").submit(function() {
+
+$(".registerForm").submit(function(event) {
   event.preventDefault();
   $("#depositWithdrawal").show();
   $("#register").hide();
-  var inputtedName = $("#inputtedName").val();
-  var inputtedInitialDeposit = $("#initialDeposit").val();
+  inputtedName = $("#inputtedName").val();
+  inputtedInitialDeposit = parseInt($("#initialDeposit").val());
+  inputtedWithdrawlAmount = parseInt($("#withdrawalInput").val());
+  balance = inputtedInitialDeposit;
+  deposit = balance +inputtedDepositAmount;
+  $(".displayBalance").append("<li>" + balance + "</li>" + "<br>" );
+
+
+
+
+$(".depositForm").submit(function(event) {
+  event.preventDefault();
+  inputtedDepositAmount = parseInt($("#depositInput").val());
+  console.log(inputtedDepositAmount);
+  $(".displayBalance").append("<li>" + inputtedDepositAmount + "</li>" + "<br>" );
+  console.log(inputtedDepositAmount);
+});
+
+$(".withdrawalForm").submit(function(event) {
+  event.preventDefault();
+
+  $(".displayBalance").append("<li>" + balance - inputtedWithdrawlAmount + "</li>" + "<br>" );
+});
 
 
 
@@ -14,16 +41,16 @@ $(".registerForm").submit(function() {
   function Account(name, initialDeposit) {
     this.name = name;
     this.initialDeposit = initialDeposit;
+
   }
 
-  var newAccount = new Account(inputtedName, inputtedInitialDeposit)
-
-  console.log(newAccount);
 
 
 
 
-})
+});
 
 
-})
+
+
+});
